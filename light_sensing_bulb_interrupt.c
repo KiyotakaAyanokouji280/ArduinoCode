0d2110c4fa5eb9f740475ec5ff2e0ce8577b77f6
KiyotakaAyanokouji280 <132389814+KiyotakaAyanokouji280@users.noreply.github.com>
2023-12-31 15:08:15 +0530

    Update light_sensing_bulb_interrupt.c

--- light_sensing_bulb_interrupt.c
+++ light_sensing_bulb_interrupt.c
@@ -4,12 +4,12 @@
 #include <avr/interrupt.h>
 
 #define OUTPUT PD7
-
+#define F_CPU 16000000UL
 void adcsetup () {
         // Use AVCC
         // Also use a 10 bit result
         // Set channel to ADC0 -> 0000 using MUX[3:0]
-        ADMUX |= (1 << REFS1) | (0 << REFS0) | (0 << ADLAR) | (0 << MUX3)  | (0 << MUX2) | (0 << MUX1) | (0 << MUX0);
+        ADMUX |= (0 << REFS1) | (0 << REFS0) | (0 << ADLAR) | (0 << MUX3)  | (0 << MUX2) | (0 << MUX1) | (0 << MUX0);
 
         // set up clock speed to about 130khz instead of the 16Mhz clock speed
         ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
@@ -18,27 +18,12 @@ void adcsetup () {
         ADCSRA |= (1 << ADEN) | (1 << ADIE);
 }
 
-ISR(ADC_vect) {
 
-        uint8_t adclow = ADCL;
-        uint16_t analogvalue = (ADCH << 8) | (ADCL);
 
-        // light on case
-        if (analogvalue < 500) {
-                PORTD |= (1 << OUTPUT);
-        }
-        else if (analogvalue >= 500) {
-                PORTD &= ~(1 << OUTPUT);
-        }
-
-        // Start the conversion again
-        ADCSRA |= (1 << ADSC);
-
-}
-
-void main() {
+int main() {
         // Set Pin 7 on arduino as output
         DDRD |= (1 << OUTPUT);
+        DDRB |= (1 << PB5);
 
         //set global interupts
         sei();
@@ -51,5 +36,27 @@ void main() {
 
 
         return 0;
+}
+
+
+
+
+ISR(ADC_vect) {
+
+        uint8_t adclow = ADCL;
+        uint16_t analogvalue = (ADCH << 2) | (adclow >> 6);
+        PORTB |= (1 << PB5);
+
+        // light on case
+        if (analogvalue > 500) {
+                PORTD |= (1 << OUTPUT);
+        }
+        else if (analogvalue <= 500) {
+                PORTD &= ~(1 << OUTPUT);
+        }
+
+        // Start the conversion again
+        ADCSRA |= (1 << ADSC);
+
 }
 
